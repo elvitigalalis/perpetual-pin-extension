bc8c1dd71d7ff32681b3f341a4c47911ab414180
LEL16 <elvitigalalis@gmail.com>
2024-06-29 22:54:35 -0400

    Added functionality for adding URLs to the perpetuallyPinnedTab list.

--- scripts/background.js
+++ scripts/background.js
@@ -3,17 +3,17 @@ chrome.runtime.onInstalled.addListener(() => {
         if (!result.perpetuallyPinnedUrls) {
             chrome.storage.sync.set({perpetuallyPinnedUrls: []}, () => {
                 console.log("Perpetually pinned tab list is initialized, but null.")
-            })
+            });
         }
     });
 
     pinStartupTabs();
     // Pins all wanted tabs on startup / window creation.
-})
+});
 
 chrome.windows.onCreated.addListener(() => {
     pinStartupTabs();
-})
+});
 
 // FIXME- if tab is already pinned, it will delete the new tab and navigate to the pinned tab.
 chrome.tabs.onCreated.addListener((tab) => {
@@ -21,7 +21,18 @@ chrome.tabs.onCreated.addListener((tab) => {
         if (result.perpetuallyPinnedUrls.includes(tab.url)) {
             chrome.tabs.update(tab.id, {pinned: true})
         }
-    })
+    });
+});
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.action === "addPerpetuallyPinnedUrl") {
+        chrome.storage.sync.get(["perpetuallyPinnedUrls"], (result) => {
+            const newUrls = [...result.perpetuallyPinnedUrls, request.url];
+            chrome.storage.sync.set({perpetuallyPinnedUrls: newUrls }, () => {
+                sendResponse({ status: "Successfully added." })
+            });
+        });
+    }
 })
 
 // If a tab in perpetuallyPinnedUrls is not open, it will open it and pin it.
@@ -33,6 +44,6 @@ function pinStartupTabs() {
                     chrome.tabs.create({url: url, pinned: true})
                 }
             })
-        })
-    })
+        });
+    });
 }
